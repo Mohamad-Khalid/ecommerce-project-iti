9c636829241317dc3f3e3b31e3f916bc1bdd5e01
sayed <sayedhassan800@gmail.com>
2024-09-15 18:46:46 +0300

    Add payment callback to update the order's state depending on payment transaction state

--- src/main/java/com/laptop/dto/payment/PaymentDTO.java
+++ src/main/java/com/laptop/dto/payment/PaymentDTO.java
@@ -2,6 +2,7 @@ package com.laptop.dto.payment;
 
 import com.laptop.entity.Customer;
 import com.laptop.entity.Order;
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,13 +26,13 @@ public class PaymentDTO {
     public PaymentDTO(Customer customer, Order order) {
         this.customer = new CustomerPaymentDTO(customer);
         this.extras = new ExtrasDTO(order.getId());
-        this.billing_data = new BillingDataDTO(customer);
+        this.billing_data = new BillingDataDTO(customer, order);
         this.amount = order.getTotalPrice();
         this.currency = "EGP";
         this.expiration = 60*15;
         this.payment_methods.add(4719614);
-        this.notification_url = "https://1503-41-37-7-190.ngrok-free" +
-                ".app/ecommerce/payment";
+        Dotenv dotenv = Dotenv.load();
+        this.notification_url = dotenv.get("TRANSACTION_PROCESSED_CALLBACK");
     }
 
 }
